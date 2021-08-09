af57922bd0847042c21bcea0c7c6e5c5ee991588
Mallory <mallorydoescoding@gmail.com>
2021-08-09 16:32:29 -0500

    All done

--- mallorys-app/src/App.js
+++ mallorys-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import UserCard from './components/UserCard';
+import './App.css';
 
 export default class App extends Component {
   constructor(props){
@@ -13,24 +14,30 @@ export default class App extends Component {
   componentDidMount(){
     fetch('https://api.github.com/users/Mad-Mal')
       .then(res => res.json())
-      .then(data => {
-        // console.log(data)
-        this.setState({ users: data })
+      .then(res => {
+        this.setState({ 
+          ...this.state,
+          users: res })
       })
       .catch(error => {
         console.log('Error with fetch', error)
       })
-    // fetch(`https://api.github.com/users/Mad-Mal/followers`)
-    //   .then(res => {
-    //     console.log(res.data)
-    //     this.setState({ followers: res.data })
-    //   })
+    fetch(`https://api.github.com/users/Mad-Mal/followers`)
+    .then(res => res.json())
+      .then(res => {
+        this.setState({ 
+          ...this.state,
+          followers: res })
+      })
+      .catch(error => {
+        console.log('Error with second fetch', error)
+      })
   };
 
   render() {
     return (
-      <div>
-        <UserCard users={this.state.users}/>
+      <div className='mainContainer'>
+        <UserCard users={this.state.users} followers={this.state.followers} />
       </div>
     )
   }
--- mallorys-app/src/components/UserCard.js
+++ mallorys-app/src/components/UserCard.js
@@ -2,27 +2,46 @@ import React, { Component } from 'react'
 
 export default class UserCard extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            users: [this.props.users]
+            users: [this.props.users],
+            followers: [this.props.followers]
         }
     }
-    // console.log()
+    
     render() {
         const userArray = this.state.users;
-        console.log(userArray)
+        const followersArray = this.state.followers;
+        // console.log(userArray)
+        // console.log(followersArray)
         return (
             <div>
-                {userArray.map((users) => {
-                    return (
-                        <div>
-                            <h2 key='id1'>{users.login}</h2>
-                            <p key='id2'>Account Type: {users.type}</p>
-                            <p key='id3'>Account URL: {users.html_url}</p>
-                            <p key='id4'>ID: {users.id}</p>
-                        </div>
-                    )
-                })}
+                <h2 className='headers'>Main User:</h2>
+                <div className='userCard'>
+                    {userArray.map(users => {
+                        return (
+                            <div key='mainId1'>
+                                <h3>Username: {users.login}</h3>
+                                <p>Account Type: {users.type}</p>
+                                <p>Account URL: {users.html_url}</p>
+                                <p>ID: {users.id}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+                <h2 className='headers'>Followers:</h2>
+                <div className='userCard'>
+                    {followersArray.map(followers => {
+                        return (
+                            <div key='mainId0'>
+                                <h3>Username: {followers.login}</h3>
+                                <p>Account Type: {followers.type}</p>
+                                <p>Account URL: {followers.html_url}</p>
+                                <p>ID: {followers.id}</p>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         )
     }
